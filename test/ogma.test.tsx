155b72cceed124e792aa3f815866755bfdb43246
Leo Nicolle <leo@linkurio.us>
2023-03-08 16:36:41 +0100

    finished update tests to vitest

---
+++ test/ogma.test.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { render } from './utils'
+
+import OgmaLib, { RawGraph } from "@linkurious/ogma";
+import { Ogma, useOgma } from "../src";
+
+const graph: RawGraph = {
+  nodes: [
+    { id: 0, attributes: { color: "red", x: 0, y: 0 } },
+    { id: 1, attributes: { color: "green", x: 25, y: 0 } },
+    { id: 2, attributes: { color: "green", x: 25, y: 0 } },
+  ],
+  edges: [
+    { source: 0, target: 1 },
+    { source: 0, target: 2 },
+  ],
+};
+
+describe("Ogma", () => {
+  let div: HTMLDivElement;
+  beforeEach(() => (div = document.createElement("div")));
+
+  it("Ogma container renders without crashing", () => {
+    render(<Ogma graph={graph} />, div);
+  });
+
+  it("Supports ref interface", () => {
+    const ref = React.createRef<OgmaLib>();
+    render(<Ogma ref={ref} graph={graph} />, div);
+    expect(ref.current).toBeDefined();
+    expect(ref.current).toBeInstanceOf(OgmaLib);
+  });
+
+  it("Ogma container renders with onReady callback", () => {
+    return new Promise(resolve => {
+      render(<Ogma graph={graph} onReady={ogma => resolve(ogma)} />, div);
+    }).then((ogma) => {
+      expect(ogma).toBeInstanceOf(OgmaLib);
+    })
+  });
+
+  it("Ogma container renders and takes options", () => {
+    const backgroundColor = "red";
+    const minimumWidth = 500;
+    return new Promise(resolve => {
+      const onReady = (ogma: OgmaLib) => {
+        const options = ogma.getOptions();
+        expect(options.backgroundColor).toBe(backgroundColor);
+        expect(options.minimumWidth).toBe(minimumWidth);
+        return resolve(null);
+      };
+      render(
+        <Ogma
+          graph={graph}
+          onReady={onReady}
+          options={{ backgroundColor, minimumWidth }}
+        />,
+        div
+      );
+    })
+  });
+
+  it("Ogma container passes the ogma instance to children", () => {
+    return new Promise(resolve => {
+      const Component = () => {
+        const ogma = useOgma();
+        expect(ogma).toBeInstanceOf(OgmaLib);
+        return resolve(null);
+      };
+      render(
+        <Ogma graph={graph}>
+          <Component />
+        </Ogma>,
+        div
+      );
+    });
+  });
+
+});
---
+++ test/styles.test.tsx
@@ -0,0 +1,158 @@
+import React from "react";
+import { render } from "react-dom";
+import { act } from "react-dom/test-utils";
+import OgmaLib from "@linkurious/ogma";
+import { Ogma, NodeStyleRule, EdgeStyleRule } from "../src";
+import graph from "./fixtures/simple_graph.json";
+
+describe("styles", () => {
+  let div: HTMLDivElement;
+  beforeEach(() => (div = document.createElement("div")));
+
+  it("Node style component renders without crashing", () => {
+    render(
+      <Ogma graph={graph}>
+        <NodeStyleRule attributes={{ color: "red" }} />
+      </Ogma>,
+      div
+    );
+  });
+
+  it("Passes node attributes", () => {
+    return new Promise(resolve => {
+      const onReady = (ogma: OgmaLib) => {
+        ogma.view.afterNextFrame().then(() => {
+          expect(ogma.getNodes().getAttribute("color")).toStrictEqual([
+            "red",
+            "red",
+            "red",
+          ]);
+          resolve(null);
+        });
+      };
+      render(
+        <Ogma graph={graph} onReady={onReady}>
+          <NodeStyleRule attributes={{ color: "red" }} />
+        </Ogma>,
+        div
+      );
+    })
+  });
+
+  it("Uses selector for NodeStyle", () => {
+    return new Promise(resolve => {
+      const onReady = (ogma: OgmaLib) => {
+        ogma.view.afterNextFrame().then(() => {
+          expect(ogma.getNodes().getAttribute("color")).toStrictEqual([
+            "red",
+            "red",
+            "green",
+          ]);
+          resolve(null);
+        });
+      };
+      render(
+        <Ogma graph={graph} onReady={onReady}>
+          <NodeStyleRule
+            attributes={{ color: "red" }}
+            selector={(node) => Number(node.getId()) < 2}
+          />
+        </Ogma>,
+        div
+      );
+    });
+
+  });
+
+  it("NodeStyle cleans up after being removed", () => {
+    const Test = ({ onReady }: { onReady: (ogma: OgmaLib) => void }) => {
+      const [style, setStyle] = React.useState<boolean>(true);
+      return (
+        <Ogma graph={graph} onReady={onReady}>
+          <button onClick={() => setStyle(!style)}>Click</button>
+          {style && <NodeStyleRule attributes={{ color: "red" }} />}
+        </Ogma>
+      );
+    };
+    let ogmaRef: OgmaLib;
+    act(() => {
+      render(<Test onReady={(ogma) => (ogmaRef = ogma)} />, div);
+    });
+    const button = div.querySelector("button") as HTMLButtonElement;
+    act(() => button.click());
+    expect(ogmaRef!.styles.getNodeRules().length).toBe(0);
+  });
+
+  it("Edge style component renders without crashing", () => {
+    render(
+      <Ogma graph={graph}>
+        <EdgeStyleRule attributes={{ color: "red" }} />
+      </Ogma>,
+      div
+    );
+  });
+
+  it("Passes edge attributes", () => {
+    return new Promise(resolve => {
+      const onReady = (ogma: OgmaLib) => {
+        ogma.view.afterNextFrame().then(() => {
+          expect(ogma.getEdges().getAttribute("color")).toStrictEqual([
+            "red",
+            "red",
+          ]);
+          resolve(null);
+        });
+      };
+      render(
+        <Ogma graph={graph} onReady={onReady}>
+          <EdgeStyleRule attributes={{ color: "red" }} />
+        </Ogma>,
+        div
+      );
+    });
+
+  });
+
+  it("Uses selector for EdgeStyle", () => {
+    return new Promise(resolve => {
+      const onReady = (ogma: OgmaLib) => {
+        ogma.view.afterNextFrame().then(() => {
+          expect(ogma.getEdges().getAttribute("color")).toStrictEqual([
+            "grey",
+            "green",
+          ]);
+          resolve(null);
+        });
+      };
+      render(
+        <Ogma graph={graph} onReady={onReady}>
+          <EdgeStyleRule
+            attributes={{ color: "green" }}
+            selector={(edge) => Number(edge.getId()) > 0}
+          />
+        </Ogma>,
+        div
+      );
+    });
+
+  });
+
+  it("EdgeStyle cleans up after being removed", () => {
+    const Test = ({ onReady }: { onReady: (ogma: OgmaLib) => void }) => {
+      const [style, setStyle] = React.useState<boolean>(true);
+      return (
+        <Ogma graph={graph} onReady={onReady}>
+          <button onClick={() => setStyle(!style)}>Click</button>
+          {style && <EdgeStyleRule attributes={{ color: "red" }} />}
+        </Ogma>
+      );
+    };
+    let ogmaRef: OgmaLib;
+    act(() => {
+      render(<Test onReady={(ogma) => (ogmaRef = ogma)} />, div);
+    });
+    const button = div.querySelector("button") as HTMLButtonElement;
+    act(() => button.click());
+    expect(ogmaRef!.styles.getEdgeRules().length).toBe(0);
+  });
+});
