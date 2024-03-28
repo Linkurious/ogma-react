a2d049c4f942c705d6523f5669c2a2e32f2a8c68
w8r <alex@milevski.co>
2024-03-28 19:16:45 +0100

    Fixed production build for UMD

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,19 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: { "process.env": { NODE_ENV: "production" } },
   build: {
     minify: true,
+    sourcemap: true,
     lib: {
       name: "OgmaReact",
       entry: "./src/index.ts",
       formats: ["es", "cjs", "umd"],
+      fileName: (format) => {
+        if (format === "umd") return `index.umd.js`;
+        if (format === "cjs") return `index.cjs`;
+        return `index.mjs`;
+      },
     },
     rollupOptions: {
       external: ["@linkurious/ogma", "react", "react-dom", "react-dom/server"],
