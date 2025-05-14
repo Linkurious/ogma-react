3b44b23005fa4c5d4c96220bcc903bd9a41f47e3
w8r <alex@milevski.co>
2025-05-14 16:37:50 +0200

    Fixed typescript conserns

--- src/hooks/useEvent.ts
+++ src/hooks/useEvent.ts
@@ -6,6 +6,7 @@ export function useEvent<
   ND = unknown,
   ED = unknown,
   K extends EventNames<ND, ED> = EventNames<ND, ED>
+  // @ts-expect-error evtName is used to infer the type of the event
 >(eventName: K, handler: (event: EventTypes<ND, ED>[K]) => void) {
   const callback = useCallback(handler, []);
 
