453c10d91b36247a84fa89c0753230eb0989074f
Jeremy Zheng <jeremyea9zg3@oUTLOok.fr>
2025-07-09 17:05:44 +0200

    experimenting with e2e tests

--- test/e2e/tests/example.spec.ts
+++ test/e2e/tests/example.spec.ts
@@ -12,7 +12,27 @@ test('has title', async ({ page }) => {
 
 test('waits for the graph to load', async ({ page }) => {
   // Wait for the graph to load
-  await new Promise(r => setTimeout(r, 2000));
-  await expect(page).toHaveScreenshot();
+  await new Promise(r => setTimeout(r, 5000));
+  await expect(page).toHaveScreenshot('initial-state.png');
   
+  const pos = await page.evaluate(() => {
+    const ogma = window.ogma;
+    const position = ogma.getNodes().get(0).getPosition();
+    return ogma.view.graphToScreenCoordinates(position);
+  });
+  await page.mouse.move(pos.x, pos.y);
+  await new Promise(r => setTimeout(r, 1500));
+  await expect(page).toHaveScreenshot('node-hovered.png');
+  // screenshot for popup?
+
+  await page.getByTitle("Show controls").click();
+  await page.getByText("Add node").click();
+  await page.mouse.click(50, 50);
+  await expect(page).toHaveScreenshot('node-added.png');
+
+  await page.getByTitle("Show controls").click();
+  await page.getByText("Use class").click();
+  await page.getByText("Add node").click();
+  await page.mouse.click(50, 50);
+  await expect(page).toHaveScreenshot('node-added-with-class.png');
 });
