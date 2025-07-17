eab32c40f4054a2f6131bc92b1c195c23f79158e
Jeremy Zheng <jeremyea9zg3@oUTLOok.fr>
2025-07-17 10:45:16 +0200

    trying to add the test results

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,8 +25,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    // ["junit", { outputFile: "reports/html/e2e/report.xml" }],
-    ["html", { outputFolder: "reports/html/e2e/" }]
+    ["junit", { outputFile: "reports/e2e/junit-test-results.xml" }],
+    ["html", { outputFolder: "reports/e2e/html" }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
