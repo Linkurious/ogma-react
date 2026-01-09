7793ec8cb6da17c1c1c2912fc720d9efb421d574
Ebernn <r1.bernier@orange.fr>
2026-01-09 16:34:13 +0100

    Fix the drilldown test

--- test/unit/transformations/drilldown.test.tsx
+++ test/unit/transformations/drilldown.test.tsx
@@ -11,14 +11,13 @@ describe("Drilldown", () => {
   it("Can be disabled by default and then enabled", async () => {
     const drilldownRef = createRef<Drilldown<any, any>>();
     render(<DrilldownTest ref={drilldownRef} disabled={true} />, div);
-    await new Promise((r) => setTimeout(r, 1000));
     await (ref.current as OgmaLib).transformations.afterNextUpdate();
     const node0 = (ref.current as OgmaLib).getNode(0)!;
     drilldownRef.current?.drill(node0);
     expect(ref.current?.getNodes().getId()).toEqual([0, 1, 2]);
     await act(() => userEvent.click(screen.getByText("toggle")));
-    drilldownRef.current?.drill(node0);
     await ref.current?.transformations.afterNextUpdate();
-    expect(ref.current?.getNodes().getId()).toEqual([1, 2, "0 - 2", "0 - 3", "0 - 4"]);
+    await ref.current?.transformations.afterNextUpdate();
+    expect(ref.current?.getNodes().getId()).toEqual(["0 - 1", 1, 2, "0 - 2", "0 - 3", "0 - 4", "ogma-group-0"]);
   });
 });
